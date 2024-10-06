59fe121fd2897d1f1827711f7172e50c8c1db301
Fede Lizondo <fedealiza@gmail.com>
2024-10-06 17:35:17 -0300

    update request

--- request-app/request_app/Entity/ParserInterface.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class ParserInterface(ABC):
-
-    @abstractmethod
-    def parse_data(self, data:str) -> dict:
-        return NotImplementedError("This method should be implemented in subclasses")
